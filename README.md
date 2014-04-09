7ee754911ff1e360218d7081da3a9f994c153d90
chetanme <chetanme@usc.edu>
2014-04-09 01:08:26 -0700

    Batch changes

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-division-page-batch
-===================
+# division-page-batch
 
-Karma batch module for division page
+A Node.js library script to easily invoke Karma in batch mode.
+
+### Installation
+
+You need to have [Node.js](http://nodejs.org/) installed. Then download `division-page-batch` and execute the following command in the directory where you cloned this repository. You need to do this to download additional packages.
+
+`npm install date-format-lite sys exec-sync shred optimist request async cron time`
+
+### Usage
+
+Typical invocation: `node karma-batch.js <spec-file>` OR `node karma-batch.js --cron <spec-file>`
+
+Type `node karma-batch.js -h` to see the available options.
+
+### Sample Specification File
+You specify all the parameters to your batch processing in a JSON file.
+You should put all your input files in one directory and all your model files also in one directory.
+
+You can optionally post the generated RDF files to a SPARQL endpoint.
+
+```
+{
+	karmaHome : "/Users/szekely/Web-Karma/" ,
+	modelsDir : "/Users/szekely/github/division-page-data/models" ,
+	filesDir : "/Users/szekely/github/division-page-data/datasets" ,
+	rdfDir : "/Users/szekely/github/division-page-data/rdf" ,
+	baseHttpDirURL : "https://raw.github.com/chetanme/division-page-data/master/data/" ,
+	runAsCron : false ,
+	cronTimeInMin : 0 ,
+	endpoint : "http://localhost:8080/openrdf-sesame/repositories/karma_data" ,
+	clearEndpoint : false ,
+	filesAndModels : [
+		{
+			file : "events.json" , model : "events-model.txt"
+		}
+		, 
+		{
+			file : "people.json" , model : "people-model.txt"
+		}
+		,
+		{
+			file : "schedule.csv" , model : "schedule-model.txt"
+		}
+	]
+}
+```
+
+### Caveats
+
+- The implementation of the functions to POST the generated RDF to a triple store is very naive as it loads the whole RDF file into memory. 
+    - A better implementation is welcome.
+- Error checking is rudimentary.
+- Does not show console output from RDF generation process. If something goes wrong you will not get a file, but will not get an error message. 
+    - I don't know how to capture the output of the child process. Please let me know if you know how to do this.
---
+++ karma-batch.js
@@ -0,0 +1,178 @@
+var fs = require('fs')
+  , dateFormat = require("date-format-lite")
+  , sys = require('sys')
+  , exec  = require('child_process').exec
+  , execSync = require("exec-sync")
+  , Shred = require("shred")
+  , optimist = require('optimist')
+  , request = require('request')
+  , async = require('async')
+  , cronJob = require('cron').CronJob
+  , time = require("time")
+;
+
+var argv = optimist
+	.usage('Apply Karma models to files and load them in a triple store\nUsage: $0 spec-file')
+	.demand(1)
+	.boolean(['noTripleStoreLoad'])
+	.describe('noTripleStoreLoad', "Don't post the generated RDF to the SPARQL endpoint")
+	.describe('h', 'Show usage/help')
+	.argv;
+
+var specFile = argv._[0];
+var rawData = fs.readFileSync(specFile);
+var spec = eval('(' + rawData + ')');
+
+if (argv.h) {
+	optimist.showHelp(fn=console.error)
+	process.exit(code=0);
+}
+
+
+function execSyncInKarmaHome(cmd) {
+	execSync("cd "+spec.karmaHome+";"+cmd);
+}
+
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+function execInKarmaHome(cmd) {
+	exec("cd "+rdfGen.karmaHome+";"+cmd, puts);
+}
+
+function getSourceType(record) {
+	var result = record.type;
+	if (result) return result;
+
+	return record.file.split('.').pop().toUpperCase();
+}
+
+function basename(input) {
+   return input.split(/\.[^.]+$/)[0];
+}
+
+function getRdfFile(record, spec) {
+	return spec.rdfDir + '/' + basename(record.file) + '.n3'
+}
+
+function runOfflineRdfGeneratorOnce(record, spec) {
+	var cmd =
+		'mvn exec:java -Dexec.mainClass="edu.isi.karma.rdf.OfflineRdfGenerator" -Dexec.args="'
+		+ ' --sourcetype ' + getSourceType(record)
+		+ ' --filepath ' + spec.filesDir + '/' + record.file + ''
+		+ ' --modelfilepath ' + spec.modelsDir + '/' + record.model + ''
+		+ ' --sourcename ' + basename(record.file) + ''
+		+ ' --outputfile ' + getRdfFile(record, spec) + '"'
+		; 
+	console.log("running: " + cmd);
+	execSyncInKarmaHome(cmd);
+}
+
+function runOfflineRdfGenerator(spec) {
+	if (spec.endpoint && spec.clearEndpoint) {
+		clearEndpoint(spec);
+	}
+
+	spec.filesAndModels.map(function(record) {
+		console.log("record:"+record);
+
+		var runGenerator = false;
+		async.series([
+			function (callback) {
+				var req = request(spec.baseHttpDirURL + record.file)
+							
+				req.pipe(fs.createWriteStream(spec.filesDir + '/temp-' + record.file + ''));
+
+				req.on ("end", function() {
+					callback();
+				});
+			}, function (callback) {
+				var stats = fs.statSync(spec.filesDir + '/' + record.file + '');
+				var tStats = fs.statSync(spec.filesDir + '/temp-' + record.file + '');
+
+				tStats["size"] += 1;
+
+				if (stats["size"] != tStats["size"]) {
+					fs.renameSync(spec.filesDir + '/temp-' + record.file + '', spec.filesDir + '/' + record.file + '');
+					runGenerator = true;
+				} else {
+					fs.delete(spec.filesDir + '/temp-' + record.file + '');
+				}
+
+				callback();
+			}, function (callback) {
+				if (runGenerator) {
+					runOfflineRdfGeneratorOnce(record, spec);
+				} else {
+					console.log("Rdf Generation not required for " + record.file);
+				}
+				callback();
+			}, function () {
+				if (spec.endpoint && !argv.noTripleStoreLoad) {
+					console.log('Loading data to ' + spec.endpoint);
+					postRdfToEndpointOnce(record, spec);
+				}
+			}
+		]);
+	});
+}
+
+function postRdfToEndpointOnce(record, spec) {
+	var shred = new Shred();
+
+	fs.readFile(getRdfFile(record, spec), 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("posting: "+getRdfFile(record, spec));
+		var req = shred.post({
+	    url: spec.endpoint+'/statements',
+	    headers: {
+	      "Content-Type": 'text/turtle'
+	    },
+	    content: data,
+	    on: {
+	      // You can use response codes as events
+	      204: function(response) {
+	      	console.log("response for "+getRdfFile(record, spec));
+	      	console.log("... success (204)");
+	      },
+	      // Any other response means something's wrong
+	      response: function(response) {
+	        console.log("Oh noaaa!");
+	        console.log(response);
+	      }
+	    }
+	  });
+  });
+}
+
+function clearEndpoint(spec) {
+	var shred = new Shred();
+	console.log("clearing: "+spec.endpoint);
+	var req = shred.delete({
+    url: spec.endpoint+'/statements',
+    on: {
+      // You can use response codes as events
+      204: function(response) {
+      	console.log("response for clearing "+spec.endpoint);
+      	console.log("... success (204)");
+      	//console.log(response.content.body);
+      },
+      // Any other response means something's wrong
+      response: function(response) {
+        console.log("Oh no!");
+      }
+    }
+  });
+}
+
+// Do it
+//runOfflineRdfGenerator(spec);
+
+if (spec.runAsCron) {
+	new cronJob(spec.cronTimeInMin + " * * * * *", function() {
+		runOfflineRdfGenerator(spec);		
+	}, null, true, "America/Los_Angeles");
+} else {
+	runOfflineRdfGenerator(spec);
+}
